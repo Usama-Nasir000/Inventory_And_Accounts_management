143b0a12fa61a3b39b7cbe277c867aec493a003d
muhammad_usama <un6457940@gmail.com>
2023-10-22 16:00:24 +0500

    DAtabaseScript

--- database/sql/database.sql
+++ database/sql/database.sql
@@ -1,136 +1,153 @@
-CREATE TABLE customers (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR,
-  first_name VARCHAR,
-  middle_name VARCHAR,
-  last_name VARCHAR,
-  category VARCHAR NOT NULL,
-  type VARCHAR NOT NULL,
-  email VARCHAR NOT NULL,
-  phone VARCHAR NOT NULL,
-  work_phone VARCHAR NOT NULL
+CREATE TABLE Users (
+   UserID INT PRIMARY KEY,
+   UserName VARCHAR(255),
+   UserEmail VARCHAR(100)
 );
 
-
-
-CREATE TABLE customer_details (
-  id SERIAL PRIMARY KEY,
-  currency VARCHAR NOT NULL,
-  terms VARCHAR NOT NULL,
-  amount FLOAT NOT NULL,
-  customer_id INTEGER NOT NULL,
-  FOREIGN KEY (customer_id) REFERENCES customers (id)
+CREATE TABLE Vendor (
+    UserID INT,
+    VendorID INT PRIMARY KEY,
+    Type VARCHAR(20), 
+    Name VARCHAR(255),
+    FirstName VARCHAR(255), 
+    MiddleName VARCHAR(255),
+    LastName VARCHAR(255),
+    Category VARCHAR(20), 
+    ContactNumber VARCHAR(20),
+    Email VARCHAR(100),
+    FOREIGN KEY (UserID) REFERENCES Users(UserID) 
 );
 
-
-CREATE TABLE customer_address (
-  id SERIAL PRIMARY KEY,
-  billing_address VARCHAR NOT NULL,
-  billing_city VARCHAR NOT NULL,
-  billing_zip FLOAT NOT NULL,
-  billing_state VARCHAR NOT NULL,
-  billing_country VARCHAR NOT NULL,
-  shipping_address VARCHAR NOT NULL,
-  shipping_city VARCHAR NOT NULL,
-  shipping_zip FLOAT NOT NULL,
-  shipping_state VARCHAR NOT NULL,
-  shipping_country VARCHAR NOT NULL,
-  customer_id INTEGER NOT NULL,
-  FOREIGN KEY (customer_id) REFERENCES customers (id)
+CREATE TABLE VendorContactPerson (
+    UserID INT,
+    VendorID INT,
+    CPFirstName VARCHAR(255), 
+    CPLastName VARCHAR(255), 
+    CPEmail VARCHAR(255), 
+    CPPhone INT,
+    CPJobRole VARCHAR(255),
+    FOREIGN KEY (VendorID) REFERENCES Vendor(VendorID),
+    FOREIGN KEY (UserID) REFERENCES Users(UserID)
 );
 
-
-
-CREATE TABLE customer_contact_person (
-  id SERIAL PRIMARY KEY,
-  contact_first_name VARCHAR NOT NULL,
-  contact_last_name VARCHAR NOT NULL,
-  contact_email VARCHAR NOT NULL,
-  contact_phone VARCHAR NOT NULL,
-  contact_job_role VARCHAR NOT NULL,
-  customer_id INTEGER NOT NULL,
-  FOREIGN KEY (customer_id) REFERENCES customers (id)
+CREATE TABLE Customer (
+    UserID INT,
+    CustomerID INT PRIMARY KEY,
+    Type VARCHAR(20), 
+    Name VARCHAR(255),
+    FirstName VARCHAR(255),
+    MiddleName VARCHAR(255),
+    LastName VARCHAR(255),
+    Category VARCHAR(20), 
+    ContactNumber VARCHAR(20),
+    Email VARCHAR(100),
+    FOREIGN KEY (UserID) REFERENCES Users(UserID)
 );
 
-
-
-
-
-
-CREATE TABLE vendors (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR,
-  first_name VARCHAR,
-  middle_name VARCHAR,
-  last_name VARCHAR,
-  category VARCHAR NOT NULL,
-  type VARCHAR NOT NULL,
-  email VARCHAR NOT NULL,
-  phone VARCHAR NOT NULL,
-  work_phone VARCHAR NOT NULL
+CREATE TABLE CustomerContactPerson (
+    UserID INT,
+    CustomerID INT,
+    CPFirstName VARCHAR(255), 
+    CPLastName VARCHAR(255), 
+    CPEmail VARCHAR(255), 
+    CPPhone INT,
+    CPJobRole VARCHAR(255),
+    FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID),
+    FOREIGN KEY (UserID) REFERENCES Users(UserID)
 );
 
-
-
-CREATE TABLE vendor_details (
-  id SERIAL PRIMARY KEY,
-  currency VARCHAR NOT NULL,
-  terms VARCHAR NOT NULL,
-  amount FLOAT NOT NULL,
-  vendor_id INTEGER NOT NULL,
-  FOREIGN KEY (vendor_id) REFERENCES vendors (id)
+CREATE TABLE VendorBillingAddresses (
+    UserID INT,
+    VendorID INT,
+    AddressID SERIAL PRIMARY KEY,
+    Address TEXT,
+    State TEXT, 
+    City TEXT,
+    Country TEXT,
+    ZipCode TEXT,
+    IsDefault BOOLEAN,
+    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+    FOREIGN KEY (VendorID) REFERENCES Vendor(VendorID)
+);
+CREATE TABLE VendorShippingAddresses (
+    UserID INT,
+    VendorID INT,
+    AddressID SERIAL PRIMARY KEY,
+    Address TEXT,
+    State TEXT, 
+    City TEXT,
+    Country TEXT,
+    ZipCode TEXT,
+    IsDefault BOOLEAN,
+    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+    FOREIGN KEY (VendorID) REFERENCES Vendor(VendorID)
 );
 
-
-
-
-
-CREATE TABLE vendor_contact_person (
-  id SERIAL PRIMARY KEY,
-  contact_first_name VARCHAR NOT NULL,
-  contact_last_name VARCHAR NOT NULL,
-  contact_email VARCHAR NOT NULL,
-  contact_phone VARCHAR NOT NULL,
-  contact_job_role VARCHAR NOT NULL,
-  vendor_id INTEGER NOT NULL,
-  FOREIGN KEY (vendor_id) REFERENCES vendors (id)
+CREATE TABLE CustomerBillingAddresses (
+    UserID INT,
+    VendorID INT,
+    AddressID SERIAL PRIMARY KEY,
+    Address TEXT,
+    State TEXT, 
+    City TEXT,
+    Country TEXT,
+    ZipCode TEXT,
+    IsDefault BOOLEAN,
+    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+    FOREIGN KEY (VendorID) REFERENCES Vendor(VendorID)
+);
+CREATE TABLE CustomerShippingAddresses (
+    UserID INT,
+    VendorID INT,
+    AddressID SERIAL PRIMARY KEY,
+    Address TEXT,
+    State TEXT, 
+    City TEXT,
+    Country TEXT,
+    ZipCode TEXT,
+    IsDefault BOOLEAN,
+    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+    FOREIGN KEY (VendorID) REFERENCES Vendor(VendorID)
 );
 
-CREATE TABLE vendor_addresses (
-  id SERIAL PRIMARY KEY,
-  vendor_id INTEGER NOT NULL,
-  address_type VARCHAR NOT NULL,
-  address_details JSONB,
-  FOREIGN KEY (vendor_id) REFERENCES vendors (id)
+CREATE TABLE Item (
+    UserID INT,
+    ItemID INT PRIMARY KEY,
+    ItemName VARCHAR(255), 
+    ItemCode VARCHAR(100),
+    PurchasePrice INT, 
+    SalesPrice INT, 
+    Currency VARCHAR(100),
+    Stock INT,
+    FOREIGN KEY (UserID) REFERENCES Users(UserID)
 );
 
+CREATE TABLE Purchase (
+    PurchaseOrderNumber INT PRIMARY KEY,
+    PurchaseDescription TEXT, 
+    TimeDate TIMESTAMP, 
+    TotalBill INT, 
+    AmountDues INT,
+    CommittedDate TIMESTAMP,
+    VendorID INT, 
+    ItemID INT, 
+    UserID INT,
+    FOREIGN KEY (VendorID) REFERENCES Vendor(VendorID),
+    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+    FOREIGN KEY (ItemID) REFERENCES Item(ItemID)
+);
 
-CREATE TABLE item (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR NOT NULL,
-  code VARCHAR NOT NULL,
-  price FLOAT NOT NULL,
-  category VARCHAR NOT NULL,
-  status VARCHAR NOT NULL,
-  cogs_account VARCHAR NOT NULL,
-  inventory_account VARCHAR NOT NULL,
-  income_account VARCHAR NOT NULL,
-  selected_costing_option VARCHAR NOT NULL,
-  selected_vendor VARCHAR NOT NULL,
-  drop_shipping BOOLEAN NOT NULL,
-  purchase_description TEXT,
-  selected_locations TEXT[],
-  sales_description TEXT,
-  sales_price FLOAT NOT NULL,
-  price_level_name VARCHAR NOT NULL,
-  qty_1 INT NOT NULL,
-  price_1 FLOAT NOT NULL,
-  qty_2 INT NOT NULL,
-  price_2 FLOAT NOT NULL,
-  qty_3 INT NOT NULL,
-  price_3 FLOAT NOT NULL,
-  qty_4 INT NOT NULL,
-  price_4 FLOAT NOT NULL,
-  qty_5 INT NOT NULL,
-  price_5 FLOAT NOT NULL
+CREATE TABLE Sales (
+    SalesOrderNum INT PRIMARY KEY, 
+    SalesDescription TEXT, 
+    TimeDate TIMESTAMP, 
+    TotalBill INT, 
+    AmountDues INT,
+    CommittedDate TIMESTAMP,
+    CustomerID INT, 
+    ItemID INT, 
+    UserID INT,
+    FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID),
+    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+    FOREIGN KEY (ItemID) REFERENCES Item(ItemID)
 );
