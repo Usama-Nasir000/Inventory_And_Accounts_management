14e23e308d92e4dc320d472236f3020da7192896
muhammad_usama <un6457940@gmail.com>
2024-01-08 19:17:25 +0500

    DockerFile update

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,15 @@
-FROM node:10.15.3-alpine as builder
-WORKDIR /todo_api
-COPY . ./
-RUN yarn install
-RUN yarn build
+FROM node:18.14.2
+WORKDIR /my_app
+ADD package.json /my_app/package.json 
+COPY . /my_app/
+RUN npm install --force
+CMD [ "sh", "-c", "npm start" ]
+EXPOSE 4000
+# RUN yarn build
 
-FROM node:10.15.3-alpine
-WORKDIR /todo_api
-COPY --from=builder /todo_api ./
-RUN yarn install --production=true
-EXPOSE 8080
-ENTRYPOINT ["yarn", "serve"]
+# FROM node:10.15.3-alpine
+# WORKDIR /todo_api
+# COPY --from=builder /todo_api ./
+# RUN yarn install --production=true
+# EXPOSE 8080
+# ENTRYPOINT ["yarn", "serve"]
--- src/server.js
+++ src/server.js
@@ -1,45 +1,113 @@
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./graphql/schema/schema');
+  const { ApolloServer } = require('apollo-server');
+  const typeDefs = require('./graphql/schema/schema');
 
 
-const itemResolver = require('./graphql/resolver/ItemResolver');
-const customerResolver = require('./graphql/resolver/CustomerResolver');
-const vendorResolver = require('./graphql/resolver/VendorResolver');
-const sales_orderResolver = require('./graphql/resolver/sales_order');
-const purchase_orderResolver = require('./graphql/resolver/purchase_order');
+  const itemResolver = require('./graphql/resolver/ItemResolver');
+  const customerResolver = require('./graphql/resolver/CustomerResolver');
+  const vendorResolver = require('./graphql/resolver/VendorResolver');
+  const sales_orderResolver = require('./graphql/resolver/sales_order');
+  const purchase_orderResolver = require('./graphql/resolver/purchase_order');
 
 
-const resolvers = { 
-  Query: {
-    ...itemResolver.Query,
-    ...customerResolver.Query,
-    ...vendorResolver.Query,
-    ...sales_orderResolver.Query,
-    ...purchase_orderResolver.Query,
-  },
-  Mutation: {
-    ...itemResolver.Mutation,
-    ...customerResolver.Mutation,
-    ...vendorResolver.Mutation,
-    ...sales_orderResolver.Mutation,
-    ...purchase_orderResolver.Mutation,
-  },
-};
+  const resolvers = { 
+    Query: {
+      ...itemResolver.Query,
+      ...customerResolver.Query,
+      ...vendorResolver.Query,
+      ...sales_orderResolver.Query,
+      ...purchase_orderResolver.Query,
+    },
+    Mutation: {
+      ...itemResolver.Mutation,
+      ...customerResolver.Mutation,
+      ...vendorResolver.Mutation,
+      ...sales_orderResolver.Mutation,
+      ...purchase_orderResolver.Mutation,
+    },
+  };
 
 
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  // rootValue: db,
-  // tracing: true
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
 
-});
+  });
 
-// server.listen({ host: '0.0.0.0', port: 4000 }).then(({ url }) => {
-//   console.log(`Server running at ${url}`);
+
+  server.listen(4000).then(({ url }) => {
+    console.log(`Server running at ${url}`);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fs = require('fs');
+// const https = require('https');
+// const { ApolloServer } = require('apollo-server-express');
+// const express = require('express');
+// const typeDefs = require('./graphql/schema/schema');
+
+// const itemResolver = require('./graphql/resolver/ItemResolver');
+// const customerResolver = require('./graphql/resolver/CustomerResolver');
+// const vendorResolver = require('./graphql/resolver/VendorResolver');
+// const sales_orderResolver = require('./graphql/resolver/sales_order');
+// const purchase_orderResolver = require('./graphql/resolver/purchase_order');
+
+// const resolvers = { 
+//   Query: {
+//     ...itemResolver.Query,
+//     ...customerResolver.Query,
+//     ...vendorResolver.Query,
+//     ...sales_orderResolver.Query,
+//     ...purchase_orderResolver.Query,
+//   },
+//   Mutation: {
+//     ...itemResolver.Mutation,
+//     ...customerResolver.Mutation,
+//     ...vendorResolver.Mutation,
+//     ...sales_orderResolver.Mutation,
+//     ...purchase_orderResolver.Mutation,
+//   },
+// };
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
 // });
 
-server.listen().then(({ url }) => {
-  console.log(`Server running at ${url}`);
-});
+// const app = express();
+
+// const httpsOptions = {
+//   key: fs.readFileSync('path/to/private-key.pem'),
+//   cert: fs.readFileSync('path/to/certificate.pem'),
+// };
+
+
+// const PORT = 4000;
+// https.createServer(httpsOptions, app).listen(PORT, () => {
+//   console.log(`Server running at https://0.0.0.0:${PORT}/graphql`);
+// });
+
+// // Start the Apollo Server before applying middleware
+// async function startServer() {
+//   await server.start();
+//   server.applyMiddleware({ app });
+  
+//   const PORT = 4000;
+
+//   app.listen(PORT, () => {
+//     console.log(`Server running at http://0.0.0.0:${PORT}/graphql`);
+//   });
+// }
+
+// startServer();
